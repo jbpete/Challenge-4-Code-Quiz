2d321ffd1848621fa38bfd6e484c58b91dceed13
Jack P <jackpeterson99528@gmail.com>
2023-07-21 18:37:59 -0400

    updated a ton of functionality

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,8 +2,8 @@ var timeEl = document.querySelector(".time")
 
 var gamePoints = 0;
 
-var questionNumber = 0;
-
+var questionNumber = 0
+var questionArrayNumber = 0;
 var secondsLeft = 90;
 var timerInterval = null;
 
@@ -34,17 +34,12 @@ var questionInfo = [
     c: "Javascript is used to add functionality and logic to your application",
     d: "All of the above",
     correct: "c",
-},
-
-{
-    question: "Game End"
-
-
 }
 ]
 
 function appendQuestion (array, questionNumber) {
-    $("#header").text("Question " + questionNumber);
+    $("#header").text("Question" + questionNumber);
+    $("#points").text("Points:" + gamePoints);
     $("#questions").text(array.question);
 
     $("#question1").text(array.a);
@@ -62,42 +57,103 @@ function showPage (pageName) {
 
 showPage(".one")
 
+
+
 $("#start_btn").click(function () {
     showPage(".two")
     setTime()
-    questionNumber = 0
-    appendQuestion(questionInfo[0], questionNumber + 1)
-
+    questionNumber++;
+    //questionArrayNumber++;
+    appendQuestion(questionInfo[0], questionNumber)
+    console.log(questionNumber)
+    console.log("questionArrayNumber", questionArrayNumber)
 
 })
+
 
 
 
 $("#next_btn").click(function () {
-    var selectedAnswer = $("input[type='radio']:checked").val();
-    questionNumber++;
-    appendQuestion(questionInfo[questionNumber], questionNumber + 1)
-    console.log(questionNumber)
-    console.log(gamePoints)
-    if (questionNumber > 2) {
-        endGame();
-    }
-    if (selectedAnswer !== questionInfo[questionNumber].correct) {
-        secondsLeft -= 10;
-    } else {
-        gamePoints += 10;
-        $("#points").text("Points: " + gamePoints);
-      }
-      console.log(gamePoints)
-})
 
-var name = document.getElementById("name").innerHTML
-var email = document.getElementById("email").innerHTML
+var selectedAnswer = $("input[name='questionStyle']:checked").val()
+questionNumber++;
+
+console.log("Question Number",questionNumber)
+console.log("Seconds Left", secondsLeft)
+console.log("questionArrayNumber", questionArrayNumber)
+
+// if (questionNumber > 3) {
+//     console.log("end game")
+//     endGame();
+
+if (selectedAnswer === questionInfo[questionArrayNumber].correct) {
+    console.log("correct")
+    gamePoints += 10;
+    questionArrayNumber++;
+    console.log("selectedAnswer",selectedAnswer)
+    console.log("Correct answer from array", questionInfo[questionArrayNumber].correct)
+    appendQuestion(questionInfo[questionArrayNumber], questionNumber)
+    console.log(questionNumber)
+} else if (selectedAnswer != questionInfo[questionArrayNumber].correct) {
+    console.log("incorrect")
+    secondsLeft -= 10;
+    console.log("selectedAnswer",selectedAnswer)
+    console.log("Correct answer from array", questionInfo[questionArrayNumber].correct)
+    appendQuestion(questionInfo[questionArrayNumber], questionNumber)
+} else if (questionNumber > 3) {
+    endGame();
+} else {
+    console.log('Im at the end of the line buddy')
+}
+})  
+
+
+// console.log("question Number",questionNumber)
+// console.log("game Points", gamePoints)
+// if (questionNumber > 2) {
+//     console.log("end game")
+//     endGame();
+// }
+// if (selectedAnswer !== questionInfo[questionNumber].correct) {
+//     console.log("incorrect")
+//     secondsLeft -= 10;
+// } else {
+//     console.log("correct")
+//     gamePoints += 10;
+//     $("#points").text("Points: " + gamePoints);
+// }
+    
+    
+    
+    
+    
+    
+    
+//     console.log(questionNumber)
+//     console.log(gamePoints)
+//     var selectedAnswer = $("input[type='radio']:checked").val();
+//     questionNumber++;
+//     appendQuestion(questionInfo[questionNumber], questionNumber + 1)
+//     console.log(selectedAnswer)
+//     if (questionNumber >= questionInfo.length - 1) {
+//         endGame();
+//     } else if (selectedAnswer !== questionInfo[questionNumber].correct) {
+//         secondsLeft -= 10;
+//         console.log('timeScore')
+//     } else {
+//         gamePoints += 10;
+//         $("#points").text("Points: " + gamePoints);
+//         console.log('score')
+//       }
+// })
+
+var name1 = document.getElementById("name")
+var email = document.getElementById("email")
 
 $("#submit_btn").click(function () {
 
-    localStorage.setItem("Name", name)
-    localStorage.setItem("Email", email)
+    localStorage.setItem("Name", name1.value)
+    localStorage.setItem("Email", email.value)
     localStorage.setItem("Points", gamePoints)
 
 })
@@ -122,4 +178,5 @@ function setTime() {
 function endGame() {
     showPage(".three")
     clearInterval(timerInterval)
+    console.log('end game func ran')
 }
